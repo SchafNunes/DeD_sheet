df2a78242d47b59cf66651079e231ac7919cf8ab
João Nunes <joaovitorschafriva@gmail.com>
2024-05-19 11:10:56 -0300

    Ajustando tipo de dado e inserindo valores na Class_Features

--- src/models/Class_Features.js
+++ src/models/Class_Features.js
@@ -14,7 +14,7 @@ const Class_Features = sequelize.define('class_features', {
         allowNull: false
     },
     description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     level: {
--- src/models/Input_Dados.js
+++ src/models/Input_Dados.js
@@ -1,4 +1,5 @@
 import Classes from "./Classes";
+import Class_Features from "./Class_Features";
 
 const classes = [
     { name: 'Bárbaro', description: 'Os bárbaros são guerreiros ferozes de uma tribo, impulsionados pela fúria e pela força bruta em combate.' },
@@ -14,10 +15,68 @@ const classes = [
     { name: 'Feiticeiro', description: 'Os feiticeiros possuem uma magia inata, derivada de uma linhagem mágica ou de um evento misterioso em suas vidas.' },
     { name: 'Bruxo', description: 'Os bruxos obtêm seus poderes através de pactos com seres extraplanares, ganhando habilidades únicas e misteriosas.' }
 ];
- 
+
+const class_features = [
+    // Características do Bárbaro
+    {
+        name: 'Fúria', 
+        description: 'No 1º nível, você pode entrar em fúria como uma ação bônus. Enquanto em fúria, você causa mais dano com ataques corpo a corpo.', 
+        class_id: 1, 
+        level: 1
+    },
+    {
+        name: 'Defesa sem Armadura', 
+        description: 'Quando você não estiver vestindo armadura, sua CA será 10 + seu modificador de Destreza + seu modificador de Constituição.', 
+        class_id: 1, 
+        level: 1
+    },
+    {
+        name: 'Ataque Imprudente', 
+        description: 'A partir do 2º nível, você pode atacar imprudentemente, concedendo vantagem aos seus ataques corpo a corpo, mas ataques contra você também terão vantagem até o próximo turno.', 
+        class_id: 1, 
+        level: 2
+    },
+    {
+        name: 'Sentir Perigo', 
+        description: 'No 2º nível, você ganha vantagem em testes de resistência de Destreza contra efeitos que você possa ver, como armadilhas e magias.', 
+        class_id: 1, 
+        level: 2
+    },
+
+    // Características do Bardo
+    {
+        name: 'Inspiração de Bardo', 
+        description: 'Você pode inspirar os outros através de palavras ou música. Usar uma inspiração de bardo consome uma de suas utilizações. Um aliado inspirado pode adicionar o dado de inspiração a uma jogada de ataque, teste de habilidade ou teste de resistência.', 
+        class_id: 2, 
+        level: 1
+    },
+    {
+        name: 'Canção de Descanso', 
+        description: 'Começando no 2º nível, você pode usar música para ajudar a revitalizar seus aliados durante um descanso curto. Se você ou quaisquer criaturas amigáveis que possam ouvir sua performance recuperarem pontos de vida ao final do descanso curto, cada uma dessas criaturas recupera pontos de vida adicionais.', 
+        class_id: 2, 
+        level: 2
+    },
+    {
+        name: 'Mão Amiga', 
+        description: 'No 2º nível, sua capacidade de inspirar os outros é melhorada. Quando uma criatura usar um dado de inspiração de bardo de você para adicionar à rolagem de um ataque, teste de habilidade ou teste de resistência, essa criatura pode rolar o dado de inspiração duas vezes e escolher o resultado mais alto.', 
+        class_id: 2, 
+        level: 2
+    },
+    {
+        name: 'Escola de Magia', 
+        description: 'No 3º nível, você escolhe uma escola de magia que representa sua prática mágica aprimorada. Sua escolha concede a você características no 3º nível e novamente no 6º e 14º níveis.', 
+        class_id: 2, 
+        level: 3
+    }
+];
+
+
+
+
 async function seedDatabase() {
     try {
         await Classes.bulkCreate(classes);
+        await Class_Features.bulkCreate(class_features);
         console.log('Dados inseridos com sucesso.');
     } catch (error) {
         console.log('Erro ao inserir dados: ', error);
