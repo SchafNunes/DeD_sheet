9c5764be503110e2a0a806fb972331307a3a9a12
João Nunes <joaovitorschafriva@gmail.com>
2024-05-23 21:25:08 -0300

    Ajustando regra de negócio

--- src/models/Characters.js
+++ src/models/Characters.js
@@ -2,7 +2,7 @@ import { DataTypes } from "sequelize";
 import {sequelize} from '../config/index.js';
 import User from './User.js';
 import Races from './Races.js'
-import Atributes from "./Atributes.js";
+import Attributes from "./Attributes.js";
 import Classes from "./Classes.js";
     
 //table id, users_id foreing_key, name, races_id foreingkey, classes.id foreingkey
@@ -14,12 +14,24 @@ const Characters = sequelize.define('characters', {
     },
     name: {
         type: DataTypes.STRING
+    },
+    user_id: {
+        type: DataTypes.INTEGER
+    },
+    race_id: {
+        type: DataTypes.INTEGER
+    },
+    attribute_id: {
+        type: DataTypes.INTEGER
+    },
+    class_id: {
+        type: DataTypes.INTEGER
     }
 });
 
 Characters.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 Characters.belongsTo(Races, { foreignKey: 'race_id', as: 'raca' });
-Characters.belongsTo(Atributes, { foreignKey: 'atribute_id', as: 'atribute' });
+Characters.belongsTo(Attributes, { foreignKey: 'attribute_id', as: 'attribute' });
 Characters.belongsTo(Classes, { foreignKey: 'class_id', as: 'class' });
 
 
