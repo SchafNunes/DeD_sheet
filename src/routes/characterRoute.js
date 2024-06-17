298bbe7553843512a92b72eaade51cc974dfbc97
leonidaspedro <leonidas@unochapeco.edu.br>
2024-06-17 17:02:08 -0300

    rota para buscar personagens utilizando o nome do usuario

--- src/routes/characterRoute.js
+++ src/routes/characterRoute.js
@@ -2,4 +2,5 @@ import characterController from "../controllers/characterController";
 
 export default (app) => {
     app.post('/character', characterController.createCharacter);
+    app.post('/characters/user', characterController.getCharactersByUser);
 }
